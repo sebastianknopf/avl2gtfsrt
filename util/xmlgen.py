3d697ee71f117378ce688aea3e3ea6fbe5585a14
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-07-17 22:55:03 +0200

    added configuration and helper script for generating sample XMLs

---
+++ util/xmlgen.py
@@ -0,0 +1,15 @@
+
+
+from itcs435.vdv.vdv435 import *
+
+gnss_physical_position_data_structure = GnssPhysicalPositionDataStructure(
+    PublisherId='XMLGEN',
+    GnssPhysicalPosition=GnssPhysicalPosition(
+        WGS84PhysicalPosition=WGS84PhysicalPosition(
+            Latitude=48.0080445,
+            Longitude=09.334598,
+        )    
+    )
+)
+
+print(gnss_physical_position_data_structure.xml())
