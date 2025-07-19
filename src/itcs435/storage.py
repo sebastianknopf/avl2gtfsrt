0095c6717214c0fd13992d882b50a6d9f58caea1
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-07-19 09:57:06 +0200

    extended Storage to process vehicle positions

--- src/itcs435/storage.py
+++ src/itcs435/storage.py
@@ -2,6 +2,7 @@ import logging
 
 from pymongo import MongoClient
 
+from itcs435.common.shared import unixtimestamp
 
 class Storage:
     def __init__(self, username: str, password: str, db_name: str = 'itcs435'):
@@ -17,6 +18,32 @@ class Storage:
     
     def update_vehicle(self, vehicle_ref: str, data: dict):
         self._db.vehicles.update_one(
+            {'vehicle_ref': vehicle_ref},
+            {'$set': data},
+            upsert=True
+        )
+
+    def get_vehicle_position(self, vehicle_ref: str) -> dict|None:
+        vehicle_position = self._db.vehicle_positions.find_one({'vehicle_ref': vehicle_ref})
+        return vehicle_position
+    
+    def update_vehicle_position(self, vehicle_ref: str, data: dict) -> None:
+        
+        # reduce last positions to the latest 10 elements
+        # remove also positions if they are older than 5 minutes
+        if 'gnss_positions' in data:
+
+            data['gnss_positions'] = data['gnss_positions'][-10:]
+
+            updated_gnss_positions: list[dict] = []
+            current_timestamp: int = unixtimestamp()
+            for gnss_position in data['gnss_positions']:
+                if gnss_position['timestamp'] > current_timestamp - 300:
+                    updated_gnss_positions.append(gnss_position)
+
+            data['gnss_positions'] = updated_gnss_positions
+        
+        self._db.vehicle_positions.update_one(
             {'vehicle_ref': vehicle_ref},
             {'$set': data},
             upsert=True
