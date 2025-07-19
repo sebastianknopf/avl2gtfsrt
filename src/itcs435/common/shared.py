875f64abb391afed9d0826c1a22c92325dc111ef
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-07-19 09:56:47 +0200

    added unixtimestamp function to shared module

--- src/itcs435/common/shared.py
+++ src/itcs435/common/shared.py
@@ -5,5 +5,9 @@ from datetime import datetime, timezone
 def isotimestamp() -> str:
     return datetime.now(timezone.utc).replace(microsecond=0).isoformat()
 
+def unixtimestamp() -> int:
+    timestamp: float = datetime.now(timezone.utc).timestamp()
+    return int(timestamp)
+
 def uid() -> str:
     return str(uuid.uuid4())
