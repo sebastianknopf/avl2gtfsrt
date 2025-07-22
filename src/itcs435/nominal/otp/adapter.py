ade4a2b4f94da1dab4b2af529d0bbcfd87726804
sebastianknopf <mail@sebastian-knopf.de>
2025-07-22 10:19:01 +0200

    removed separate class for OtpClient and moved code to OtpAdapter

--- src/itcs435/nominal/otp/adapter.py
+++ src/itcs435/nominal/otp/adapter.py
@@ -1,7 +1,86 @@
+import logging
+import requests
+
+from datetime import datetime, timezone
+
+from itcs435.common.env import is_debug
 from itcs435.nominal.baseadapter import BaseNominalAdapter
-from itcs435.nominal.otp.otpclient import OtpClient
+
 
 class OtpAdapter(BaseNominalAdapter):
 
-    def get_trip_candidates(self, latitude, longitude):
-        raise NotImplementedError()
+    def __init__(self, endpoint: str):
+        self._endpoint = endpoint
+        
+    def get_trip_candidates(self, lat: float, lon: float) -> dict:
+        query = """
+        query TripCandidates($lat: Float!, $lon: Float!, $startTime: DateTime!) {
+          nearest(latitude: $lat, longitude: $lon, maximumDistance: 200, filterByPlaceTypes:stopPlace) {
+            edges {
+              node {
+                distance,
+                place {
+                  ... on StopPlace {
+                    id,
+                    estimatedCalls(startTime: $startTime, numberOfDepartures: 5) {
+                      serviceJourney {
+                        id,
+                        journeyPattern {
+                          line {
+                            id
+                          }
+                        }
+                        pointsOnLink {
+                          points
+                        }
+                        estimatedCalls {
+                          aimedArrivalTime,
+                          aimedDepartureTime,
+                            quay {
+                            id,
+                            latitude,
+                            longitude
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        """
+        
+        variables = {
+          'lat': lat,
+          'lon': lon,
+          'startTime': datetime.now(timezone.utc).isoformat()
+        }
+        
+        data: dict = self._request(query, variables)
+        return data.get('data', {}).get('nearest', {})
+        
+    def _request(self, query: str, variables: dict) -> dict:
+        try:
+            response = requests.post(
+                self._endpoint,
+                json={
+                    'query': query, 
+                    'variables': variables
+                },
+                headers={
+                    'Content-Type': 'application/json'
+                }
+            )
+            
+            response.raise_for_status()
+            
+            return response.json()
+        except Exception as ex:
+            if is_debug():
+                logging.exception(ex)
+            else:
+                logging.error(str(ex)) 
+            
+            return None
