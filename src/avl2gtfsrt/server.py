9fa9c57e2d63e776e68eb5a0b267a72ffd5e2f75
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-10-10 11:37:48 +0200

    implemented basic function for serving vehicle positions via GTFS-RT

--- src/avl2gtfsrt/server.py
+++ src/avl2gtfsrt/server.py
@@ -1,6 +1,8 @@
 import json
+import logging
 import os
 import pytz
+import uuid
 import uvicorn
 
 from datetime import datetime
@@ -12,9 +14,19 @@ from google.transit import gtfs_realtime_pb2
 from google.protobuf.json_format import ParseDict
 from math import floor
 
+from avl2gtfsrt.objectstorage import ObjectStorage
+
 class GtfsRealtimeServer():
     
     def __init__(self) -> None:
+        # connect to local MongoDB
+        mongodb_username: str = os.getenv('A2G_MONGODB_USERNAME', '')
+        mongodb_password: str = os.getenv('A2G_MONGODB_PASSWORD', '')
+
+        logging.info(f"{self.__class__.__name__}: Connecting to MongoDB ...")
+        self._object_storage: ObjectStorage = ObjectStorage(mongodb_username, mongodb_password)
+        
+        logging.info(f"{self.__class__.__name__}: Creating FastAPI instance ...")
         self._fastapi = FastAPI()
         self._api_router = APIRouter()
 
@@ -22,7 +34,38 @@ class GtfsRealtimeServer():
         self._api_router.add_api_route('/trip-updates.pbf', endpoint=self._trip_updates, methods=['GET'], name='trip_updates')
 
     async def _vehicle_positions(self, request: Request) -> Response:
-        return await self._response(request, [])
+        entities: list[dict] = list()
+        
+        vehicles: list = self._object_storage.get_vehicles()
+        for vehicle in vehicles:
+            if vehicle.get('is_technically_logged_on', False):
+                vehicle_position: dict = self._object_storage.get_vehicle_position(vehicle.get('vehicle_ref'))
+                if vehicle_position is not None:
+                    entity: dict = {
+                        'id': str(uuid.uuid4()),
+                        'vehicle': {
+                            'timestamp': vehicle_position['timestamp'], 
+                            'vehicle': {
+                                'id': vehicle.get('vehicle_ref')
+                            },
+                            'position': {
+                                'latitude': vehicle_position['latitude'],
+                                'longitude': vehicle_position['longitude']
+                            }
+                        }
+                    }
+
+                    if vehicle.get('is_operationally_logged_on', False):
+                        entity['trip'] = {
+                            'trip_id': 'test',
+                            'route_id': 'test',
+                            'start_time': '11:00:00',
+                            'start_date': 20251010
+                        }
+            
+                    entities.append(entity)
+
+        return await self._response(request, entities)
 
     async def _trip_updates(self, request: Request) -> Response:
         return await self._response(request, [])
